638d7b2aef0ac6397ca5582bc89ebb3c375b57a1
karinsetro <39471099+karinsetro@users.noreply.github.com>
2018-06-23 00:04:31 -0300

    Add files via upload

---
+++ ICTdelivery.java
@@ -0,0 +1,15 @@
+
+package ictdelivery;
+
+import ictdelivery.view.MainFrame;
+
+public class ICTdelivery {
+
+    public static void main(String[] args) {
+       
+        //open mainframe
+        new MainFrame().setVisible(true);
+        
+    }
+    
+}
